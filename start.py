0a716a2cc49826e0c188005e112d15ad5e6510df
Fr4nks <fsdevilliers@gmail.com>
2023-04-30 00:46:09 +0200

    test

--- start.py
+++ start.py
@@ -24,12 +24,13 @@ start_path = "C:\\Users\\fr4nk\\OneDrive\\Desktop\\start"
 start_command = f'cmd /k "cd /d {start_path} /env/Scripts && activate && cd /d {start_path} && genauto.py"'
 genauto_process = subprocess.Popen(start_command, shell=True)
 
+host = settings['host']
 fastapi_path = settings['fastapi_path']
-start_command = f'cmd /k "cd /d {fastapi_path} /env/Scripts && activate && cd /d {fastapi_path} &&  uvicorn src.main:app --port 5000"'
+start_command = f'cmd /k "cd /d {fastapi_path} /env/Scripts && activate && cd /d {fastapi_path} &&  uvicorn src.main:app --port 5000 {host}"'
 fastapi_process = subprocess.Popen(start_command, shell=True)
 time.sleep(20)
 os.chdir(settings['angular_path'])
-angular_process = subprocess.Popen('ng serve', shell=True)
+angular_process = subprocess.Popen('ng serve -c x300', shell=True)
 time.sleep(20)
 webbrowser.open_new(settings['web_url'])
 
