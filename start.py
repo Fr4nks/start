956d3521dee559d6b4ba18254cc540d9d18104c3
Fr4nks <fsdevilliers@gmail.com>
2023-04-30 14:58:13 +0200

    test

--- start.py
+++ start.py
@@ -30,7 +30,7 @@ start_command = f'cmd /k "cd /d {fastapi_path} /env/Scripts && activate && cd /d
 fastapi_process = subprocess.Popen(start_command, shell=True)
 time.sleep(20)
 os.chdir(settings['angular_path'])
-angular_process = subprocess.Popen(f'ng serve -c x300 {host}', shell=True)
+angular_process = subprocess.Popen(f'ng serve --configuration=x300 {host}', shell=True)
 time.sleep(20)
 webbrowser.open_new(settings['web_url'])
 
